59bfce0fcd56218d1150ed30597ea6a0b153a5e2
Lorena Peñas <lorenappff@gmail.com>
2026-04-15 20:06:14 +0200

    refactor: change name of interfaces

--- item-hexagonal/src/main/java/com/logistics/item/infrastructure/repository/ItemMongoRepository.java
+++ item-hexagonal/src/main/java/com/logistics/item/infrastructure/repository/ItemMongoRepository.java
@@ -2,9 +2,10 @@ package com.logistics.item.infrastructure.repository;
 
 import com.logistics.item.infrastructure.repository.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface ItemJpaRepository extends JpaRepository<ItemEntity, String> {
+public interface ItemMongoRepository extends MongoRepository<ItemEntity, String> {
     List<ItemEntity> findByName(String name);
 }
